79d67b5be7365e5abf63117ee9a4f83c381e4f7f
Ajay Paratmandali <ajay.paratmandali@seagate.com>
2020-07-15 04:09:48 -0600

    EOS-9768: Add cicd check for cli

--- jenkins/cicd/cortx-ha-cicd.sh
+++ jenkins/cicd/cortx-ha-cicd.sh
@@ -8,4 +8,6 @@ mkdir -p /etc/cortx/ha/
 cp -rf ${BASE_DIR}/jenkins/cicd/etc/decision_monitor_conf.json /etc/cortx/ha/decision_monitor_conf.json
 cp -rf ${BASE_DIR}/jenkins/cicd/etc/database.json /etc/cortx/ha/database.json
 
+cortxha cleanup db --node srvnode-1 || exit 1
+
 /usr/lib/ocf/resource.d/seagate/hw_comp_ra meta-data
