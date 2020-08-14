9ceaa8823573fb3b3a37df171a9fd97dcb876eda
Ajay Paratmandali <66459479+ajay-paratmandali@users.noreply.github.com>
2020-08-14 10:24:59 +0530

    Test (#63)
    * EOS-7798_stonith: Resolve stonith
    
    * Unit test for resource agent
    
    Signed-off-by: Ajay Paratmandali <ajay.paratmandali@seagate.com>
    
    Co-authored-by: ajaysrivas <66410865+ajaysrivas@users.noreply.github.com>

--- jenkins/cicd/cortx-ha-cicd.sh
+++ jenkins/cicd/cortx-ha-cicd.sh
@@ -4,9 +4,18 @@ set -x
 
 BASE_DIR=$(realpath "$(dirname $0)/../../")
 
+# Copy Backend files
+TMPHA="${BASE_DIR}"/dist/tmp/cortx/ha
+rm -rf "${TMPHA}"
+mkdir -p "${TMPHA}"
+cp -rs "$BASE_DIR"/ha/* "${TMPHA}"
+
 mkdir -p /etc/cortx/ha/
-cp -rf ${BASE_DIR}/jenkins/cicd/etc/decision_monitor_conf.json /etc/cortx/ha/decision_monitor_conf.json
-cp -rf ${BASE_DIR}/jenkins/cicd/etc/database.json /etc/cortx/ha/database.json
+cp -rf "${BASE_DIR}"/jenkins/cicd/etc/decision_monitor_conf.json /etc/cortx/ha/decision_monitor_conf.json
+cp -rf "${BASE_DIR}"/jenkins/cicd/etc/database.json /etc/cortx/ha/database.json
+
+# Perform unit test
+python3 "${TMPHA}"/test/main.py "${TMPHA}"/test/unit
 
 cortxha cleanup db --node srvnode-1 || exit 1
 
