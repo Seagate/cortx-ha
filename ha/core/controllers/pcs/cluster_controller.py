206a69e905cad6e3be205c1cc05a6c33dcd88614
Ajay Paratmandali <66459479+ajay-paratmandali@users.noreply.github.com>
2021-02-17 09:56:52 +0530

    EOS-15892: Cluster Manager Controllers (#300)
    * Cluster Manager Controllers
    
    Signed-off-by: Ajay Paratmandali <ajay.paratmandali@seagate.com>
    
    * Resolve codacy error
    
    Signed-off-by: Ajay Paratmandali <ajay.paratmandali@seagate.com>
    
    * Remove unused content
    
    Signed-off-by: Ajay Paratmandali <ajay.paratmandali@seagate.com>
    
    * Add storageset controller
    
    Signed-off-by: Ajay Paratmandali <ajay.paratmandali@seagate.com>
    
    * Modify code add storageset
    
    Signed-off-by: Ajay Paratmandali <ajay.paratmandali@seagate.com>
    
    * Improve all function as per lld
    
    Signed-off-by: Ajay Paratmandali <ajay.paratmandali@seagate.com>
    
    * Resolve comments
    
    Signed-off-by: Ajay Paratmandali <ajay.paratmandali@seagate.com>
    
    * Improve class hearichy
    
    Signed-off-by: Ajay Paratmandali <ajay.paratmandali@seagate.com>
    
    * Add note for element controller
    
    Signed-off-by: Ajay Paratmandali <ajay.paratmandali@seagate.com>

---
+++ ha/core/controllers/pcs/cluster_controller.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2021 Seagate Technology LLC and/or its Affiliates
+#
+# This program is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Affero General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <https://www.gnu.org/licenses/>. For any questions
+# about this software or licensing, please email opensource@seagate.com or
+# cortx-questions@seagate.com.
+
+from ha.core.error import HAUnimplemented
+from ha.core.controllers.pcs.pcs_controller import PcsController
+from ha.core.controllers.cluster_controller import ClusterController
+from ha.core.controllers.controller_annotation import controller_error_handler
+
+class PcsClusterController(ClusterController, PcsController):
+    """ Pcs cluster controller to perform pcs cluster level operation. """
+
+    def __init__(self):
+        """
+        Initalize pcs cluster controller
+        """
+        super(PcsClusterController, self).__init__()
+
+    @controller_error_handler
+    def start(self) -> dict:
+        """
+        Start cluster and all service.
+
+        Returns:
+            ([dict]): Return dictionary. {"status": "", "msg":""}
+                status: Succeeded, Failed, InProgress
+        """
+        raise HAUnimplemented("Cluster start operation is not implemented.")
+
+    @controller_error_handler
+    def stop(self) -> dict:
+        """
+        Stop cluster and all service.
+
+        Returns:
+            ([dict]): Return dictionary. {"status": "", "msg":""}
+                status: Succeeded, Failed, InProgress
+        """
+        raise HAUnimplemented("This operation is not implemented.")
+
+    @controller_error_handler
+    def status(self) -> dict:
+        """
+        Status cluster and all service. It gives status for all resources.
+
+        Returns:
+            ([dict]): Return dictionary. {"status": "", "msg":{}}
+                status: Succeeded, Failed, InProgress
+                msg: dict of resource and status.
+        """
+        raise HAUnimplemented("This operation is not implemented.")
+
+    @controller_error_handler
+    def standby(self) -> dict:
+        """
+        Put cluster in standby mode.
+
+        Returns:
+            ([dict]): Return dictionary. {"status": "", "msg":""}
+                status: Succeeded, Failed, InProgress
+        """
+        raise HAUnimplemented("This operation is not implemented.")
+
+    @controller_error_handler
+    def active(self) -> dict:
+        """
+        Activate all node.
+
+        Returns:
+            ([dict]): Return dictionary. {"status": "", "msg":""}
+                status: Succeeded, Failed, InProgress
+        """
+        raise HAUnimplemented("This operation is not implemented.")
+
+    @controller_error_handler
+    def node_list(self) -> dict:
+        """
+        Provide node list.
+
+        Returns:
+            ([dict]): Return dictionary. {"status": "", "msg":[]}
+                status: Succeeded, Failed, InProgress
+        """
+        raise HAUnimplemented("This operation is not implemented.")
+
+    @controller_error_handler
+    def service_list(self) -> dict:
+        """
+        Provide service list.
+
+        Returns:
+            ([dict]): Return dictionary. {"status": "", "msg":[]}
+                status: Succeeded, Failed, InProgress
+        """
+        raise HAUnimplemented("This operation is not implemented.")
+
+    @controller_error_handler
+    def storageset_list(self) -> dict:
+        """
+        Provide storageset list.
+
+        Returns:
+            ([dict]): Return dictionary. {"status": "", "msg":[]}
+                status: Succeeded, Failed, InProgress
+        """
+        raise HAUnimplemented("This operation is not implemented.")
+
+    @controller_error_handler
+    def add_node(self, nodeid: str = None, descfile: str = None) -> dict:
+        """
+        Add new node to cluster.
+
+        Args:
+            nodeid (str, optional): Provide nodeid. Defaults to None.
+            filename (str, optional): Provide descfile. Defaults to None.
+
+        Returns:
+            ([dict]): Return dictionary. {"status": "", "msg":""}
+                status: Succeeded, Failed, InProgress
+        """
+        raise HAUnimplemented("This operation is not implemented.")
+
+    @controller_error_handler
+    def add_storageset(self, nodeid: str = None, descfile: str = None) -> dict:
+        """
+        Add new storageset to cluster.
+
+        Args:
+            nodeid (str, optional): Provide nodeid. Defaults to None.
+            filename (str, optional): Provide descfile. Defaults to None.
+
+        Returns:
+            ([dict]): Return dictionary. {"status": "", "msg":""}
+                status: Succeeded, Failed, InProgress
+        """
+        raise HAUnimplemented("This operation is not implemented.")
