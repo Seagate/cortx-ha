e3ac984204468f59dcc3c25607b34c7cfdacc026
Vijaykumar Thakkar <vijaykumar.thakkar@seagate.com>
2021-04-20 07:43:37 -0600

    Added exception handling for the alert parser block
    Signed-off-by: Vijaykumar Thakkar <vijaykumar.thakkar@seagate.com>

--- ha/core/event_analyzer/parser/parser.py
+++ ha/core/event_analyzer/parser/parser.py
@@ -17,6 +17,7 @@
 
 import abc
 import json
+from ha.core.error import EventAnalyzerError
 from ha.core.system_health.model.health_event import HealthEvent
 
 class Parser(metaclass=abc.ABCMeta):
@@ -44,23 +45,28 @@ class AlertParser(Parser):
         Args:
             msg (str): Msg
         """
-        alert = json.loads(msg)
+        try:
+            alert = json.loads(msg)
 
-        event = {
-            "event_id" : alert.get('sensor_response_type').get('alert_id'),
-            "event_type" : alert.get('sensor_response_type').get('alert_type'),
-            "severity" : alert.get('sensor_response_type').get('severity'),
-            "site_id" : alert.get('sensor_response_type').get('info').get('site_id'),
-            "rack_id" : alert.get('sensor_response_type').get('info').get('rack_id'),
-            "cluster_id" : alert.get('sensor_response_type').get('info').get('cluster_id'),
-            "storageset_id" : "TBD",
-            "node_id" : alert.get('sensor_response_type').get('info').get('node_id'),
-            "host_id" : alert.get('sensor_response_type').get('host_id'),
-            "resource_type" : alert.get('sensor_response_type').get('info').get('resource_type'),
-            "timestamp" : alert.get('sensor_response_type').get('info').get('event_time'),
-            "resource_id" : alert.get('sensor_response_type').get('info').get('resource_id'),
-            "specific_info" : alert.get('sensor_response_type').get('specific_info')
-        }
+            event = {
+                "event_id" : alert['sensor_response_type']['alert_id'],
+                "event_type" : alert['sensor_response_type']['alert_type'],
+                "severity" : alert['sensor_response_type']['severity'],
+                "site_id" : alert['sensor_response_type']['info']['site_id'],
+                "rack_id" : alert['sensor_response_type']['info']['rack_id'],
+                "cluster_id" : alert['sensor_response_type']['info']['cluster_id'],
+                "storageset_id" : "TBD",
+                "node_id" : alert['sensor_response_type']['info']['node_id'],
+                "host_id" : alert['sensor_response_type']['host_id'],
+                "resource_type" : alert['sensor_response_type']['info']['resource_type'],
+                "timestamp" : alert['sensor_response_type']['info']['event_time'],
+                "resource_id" : alert['sensor_response_type']['info']['resource_id'],
+                "specific_info" : alert['sensor_response_type']['specific_info']
+            }
 
-        health_event = HealthEvent.dict_to_object(event)
+            health_event = HealthEvent.dict_to_object(event)
+
+        except Exception as e:
+            raise EventAnalyzerError(f"Failed to parse alert. Message: {msg}, Error: {e}")
+
         return health_event
