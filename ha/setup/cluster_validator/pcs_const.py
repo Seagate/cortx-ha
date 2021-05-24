4e62b6b6d5bc8d34b987abaac58f82021bdb5558
Ajay Srivastava <ajay.srivastava@seagate.com>
2021-05-24 16:26:59 +0530

    License header.

--- ha/setup/cluster_validator/pcs_const.py
+++ ha/setup/cluster_validator/pcs_const.py
@@ -1,6 +1,24 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2021 Seagate Technology LLC and/or its Affiliates
+#
+# This program is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Affero General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <https://www.gnu.org/licenses/>. For any questions
+# about this software or licensing, please email opensource@seagate.com or
+# cortx-questions@seagate.com.
+
 
 class COMMNANDS:
     PCS_STATUS_XML = "pcs status --full xml"
+
 
 class CLUSTER_ATTRIBUTES:
     NAME = "name"
@@ -30,6 +48,7 @@ class CLUSTER_ATTRIBUTES:
     RESOURCE = "resource"
     RESOURCES = "resources"
     CURRENT_DC = "current_dc"
+
 
 class RESOURCE_ATTRIBUTES:
     ID = "id"
