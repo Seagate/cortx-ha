5cd94da05c6840f72bbbca18167761dfa87d984f
indrajitzagade <63033334+indrajitzagade@users.noreply.github.com>
2021-05-28 11:47:36 +0530

    EOS-19913: Add IEM parser to event analyzer (#408)
    * Added parser for IEMs
    
    * Added test for IEM parser
    
    * Updated comments and logs
    
    * Addressed review comments
    
    Co-authored-by: ajaysrivas <66410865+ajaysrivas@users.noreply.github.com>

--- ha/const.py
+++ ha/const.py
@@ -66,6 +66,9 @@ IEM_DESCRIPTION="WS0080010001,Node, The cluster has lost $host server. System is
 # IEM DESCRIPTION string: To be removed
 IEM_DESCRIPTION="WS0080010001,Node, The cluster has lost $host server. System is running in degraded mode. For more information refer the Troubleshooting guide. Extra Info: host=$host; status=$status;"
 
+# IEM DESCRIPTION string: To be removed
+IEM_DESCRIPTION="WS0080010001,Node, The cluster has lost $host server. System is running in degraded mode. For more information refer the Troubleshooting guide. Extra Info: host=$host; status=$status;"
+
 # Mini-provisioning
 CLUSTER_CONFSTORE_NODES_KEY="nodes"
 
