bd927e84ca103fab2331889d21c6517102708e51
shindeam <63034427+shindeam@users.noreply.github.com>
2021-05-28 11:25:55 +0530

    EOS-19893: HA event analyzers IEM filter (#410)
    * EOS-19893 : iem filter for event analyzers
    
    Signed-off-by: Amol Shinde <amol.shinde@seagate.com>
    
    * EOS-19893 : fix for IEM filter test
    
    Signed-off-by: Amol Shinde <amol.shinde@seagate.com>
    
    * EOS-19893 : fix for review comments
    
    Signed-off-by: Amol Shinde <amol.shinde@seagate.com>
    
    * EOS-19893: minor fix for imports in test
    
    Signed-off-by: Amol Shinde <amol.shinde@seagate.com>
    
    * EOS-19893: fix minor issues
    
    Signed-off-by: Amol Shinde <amol.shinde@seagate.com>
    
    * EOS-19893 fix for review comments
    
    Signed-off-by: Amol Shinde <amol.shinde@seagate.com>
    
    * EOS-19893 : fix for review comments
    
    Signed-off-by: Amol Shinde <amol.shinde@seagate.com>
    
    * EOS-19893 : moved literals to const
    
    Signed-off-by: Amol Shinde <amol.shinde@seagate.com>
    
    * EOS-19893 : created alert & iem const enums
    
    Signed-off-by: Amol Shinde <amol.shinde@seagate.com>
    
    * EOS-19893: fix for review comments
    
    Signed-off-by: Amol Shinde <amol.shinde@seagate.com>
    
    Co-authored-by: ajaysrivas <66410865+ajaysrivas@users.noreply.github.com>

--- ha/core/event_analyzer/filter/filter.py
+++ ha/core/event_analyzer/filter/filter.py
@@ -31,6 +31,12 @@ class MESSAGETYPE(Enum):
     ALERT = "ALERT"
     IEM = "IEM"
 
+
+class MESSAGETYPE(Enum):
+    ALERT = "ALERT"
+    IEM = "IEM"
+
+
 class Filter(metaclass=abc.ABCMeta):
     """ Base class to filter alert """
 
