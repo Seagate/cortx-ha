1aca3979c7b841632dbb6091d0b8571800355ac3
shindeam <63034427+shindeam@users.noreply.github.com>
2021-06-01 17:24:28 +0530

    EOS-18020: Alert Generator: Design and Implement filtering mechanism (#428)
    * EOS-18020 : alert event filter mechanism implementation.
    
    Signed-off-by: Amol Shinde <amol.shinde@seagate.com>
    
    * fix for alert filter
    
    Signed-off-by: Amol Shinde <amol.shinde@seagate.com>
    
    * EOS-18020: Addresed review comments
    
    Signed-off-by: Amol Shinde <amol.shinde@seagate.com>
    
    * EOS-18020 : addresed review comments
    
    Signed-off-by: Amol Shinde <amol.shinde@seagate.com>
    
    * EOS-18020 : fix for review comment.
    
    Signed-off-by: Amol Shinde <amol.shinde@seagate.com>
    
    Co-authored-by: ajaysrivas <66410865+ajaysrivas@users.noreply.github.com>

---
+++ ha/alert/filter.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2021 Seagate Technology LLC and/or its Affiliates
+#
+# This program is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Affero General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <https://www.gnu.org/licenses/>. For any questions
+# about this software or licensing, please email opensource@seagate.com or
+# cortx-questions@seagate.com.
+
+import abc
+from cortx.utils.conf_store.conf_store import Conf
+from cortx.utils.log import Log
+import ha.alert.const as const
+from ha.core.config.config_manager import ConfigManager
+from ha.alert.alert_exceptions import AlertEventFilterError
+
+
+class Filter(metaclass=abc.ABCMeta):
+    """ Base class to filter alert """
+
+    def __init__(self):
+        """
+        Init method
+        """
+        self.crm_env = None
+        # Loads Alert event filter rules in the configuration
+        ConfigManager.load_alert_events_rules()
+
+    def initialize_crm(self, crm_env: dict):
+        self.crm_env = crm_env
+
+    @abc.abstractmethod
+    def filter_event(self):
+        """
+        Filter event based on Node, Resource and Fencing type.
+        Args:
+            msg (str): Msg
+        """
+        pass
+
+
+class AlertEventFilter(Filter):
+    """ Filter unnecessary alert. """
+
+    def __init__(self):
+        """
+        Init method
+        """
+        super().__init__()
+
+        # Get filter type and resource types list from the alert monitor rule file
+        self.alert_filter_components_list = Conf.get(const.ALERTS.PK_ALERT_EVENT_COMPONENTS)
+        self.alert_filter_modules_list = Conf.get(const.ALERTS.PK_ALERT_EVENT_COMPONENT_MODULES)
+        self.alert_filter_module_operations = Conf.get(const.ALERTS.PK_ALERT_EVENT_OPERATIONS)
+        Log.info("AlertEventFilter initialized.")
+
+    def filter_event(self):
+        """
+        Filter event based on Node, Resource and Fencing type.
+        Args:
+            msg (str): Msg
+        """
+        ha_required_alert_module = None
+        ha_required_alert_module_operation = None
+
+        if self.crm_env is None:
+            raise AlertEventFilterError(f"Identified empty events: {str(self.crm_env)}")
+
+        try:
+            alert_filter_module = self.crm_env["CRM_alert_kind"]
+            alert_filter_module_operation = self.crm_env["CRM_alert_desc"]
+
+            if alert_filter_module in self.alert_filter_modules_list and \
+                    alert_filter_module_operation in self.alert_filter_module_operations:
+                ha_required_alert_module = alert_filter_module
+                ha_required_alert_module_operation = alert_filter_module_operation
+
+            return ha_required_alert_module, ha_required_alert_module_operation
+        except Exception as e:
+            Log.error(f"Error occurred in alert event filter: {str(e)}")
+            return ha_required_alert_module, ha_required_alert_module_operation
