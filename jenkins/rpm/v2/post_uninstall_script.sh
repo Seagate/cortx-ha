d161be71a37cca0f4927cd55c315b380d29b2253
ajaysrivas <66410865+ajaysrivas@users.noreply.github.com>
2021-06-01 23:15:33 +0530

    EOS-18360: package using setuptools. (#424)
    * package using setuptools.
    
    * Trigger for codacy
    
    * codacy fixes.
    
    * Remove cortx-py-utils
    
    * review comments.
    
    * Trigger build
    
    * Corrected path for template files.
    
    * Corrected path for template files.

---
+++ jenkins/rpm/v2/post_uninstall_script.sh
@@ -0,0 +1,25 @@
+[ $1 -eq 1 ] && exit 0
+
+RES_AGENT="/usr/lib/ocf/resource.d/seagate"
+HA_INSTALL_DIR=/opt/seagate/cortx/ha
+BIN_DIR=${HA_INSTALL_DIR}/bin
+
+rm -f ${BIN_DIR}/ha_setup
+rm -f /usr/bin/ha_setup
+rm -f /usr/local/bin/ha_setup
+
+rm -f ${BIN_DIR}/cortx
+rm -f /usr/bin/cortx
+rm -f /usr/bin/cortxha
+
+rm -f ${BIN_DIR}/dynamic_fid_service_ra
+rm -f /usr/bin/dynamic_fid_service_ra
+rm -f $RES_AGENT/dynamic_fid_service_ra
+
+rm -f ${BIN_DIR}/event_analyzerd
+rm -f /usr/local/bin/event_analyzerd
+rm -f /usr/bin/event_analyzerd
+
+rm -rf ${HA_INSTALL_DIR}
+
+exit 0
