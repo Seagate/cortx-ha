4beb1c0ddb7c1296652ff00012133f4183bf5a29
Ajay Paratmandali <ajay.paratmandali@seagate.com>
2021-06-03 03:28:33 -0600

    Update PR
    Signed-off-by: Ajay Paratmandali <ajay.paratmandali@seagate.com>

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -17,7 +17,6 @@
 
 set -ex
 BASE_DIR=$(realpath "$(dirname $0)/..")
-DIST_DIR="$BASE_DIR/dist/cortx-ha"
 PROG_NAME=$(basename $0)
 HA_INSTALL_PATH="/opt/seagate/cortx/ha"
 HA2="2"
@@ -28,7 +27,6 @@ usage() {
     echo """
 usage: $PROG_NAME [-v <cortx-ha major version>] [-m <cortx-ha minor version>]
                   [-r <cortx-ha revision version>] [-b <build no>] [-k <key>]
-
 Options:
     -v : Build rpm with major version
     -m : Build rpm with minor version
@@ -69,30 +67,25 @@ cd $BASE_DIR
 echo "Using MAJOR_VERSION=${MAJ_VER} MINOR_VERSION=${MIN_VER} REVISION=${PATCH_VER} BUILD=${BUILD}"
 version="${MAJ_VER}.${MIN_VER}.${PATCH_VER}"
 
-# Cleanup
-rm -rf $BASE_DIR/dist
-
-# Copy
-mkdir -p $DIST_DIR
-cp -rf `ls -A | grep -v "dist" | grep -v ".git*"` $DIST_DIR/
-
-cd $DIST_DIR
+rm -rf ${BASE_DIR}/dist; mkdir -p ${BASE_DIR}/dist/temp
 
 # Update version in conf file
-sed -i -e "s|<VERSION>|${version}|g" ${DIST_DIR}/conf/etc/v2/ha.conf
+cp ${BASE_DIR}/conf/etc/v2/ha.conf ${BASE_DIR}/dist/temp/ha.conf
+sed -i -e "s|<VERSION>|${version}|g" ${BASE_DIR}/conf/etc/v2/ha.conf
 
-/usr/bin/env python3 setup.py bdist_rpm --version="${version}" --install-dir="${HA_INSTALL_PATH}" --release="$BUILD" \
+python3.6 setup.py bdist_rpm --version="${version}" --install-dir="${HA_INSTALL_PATH}" --release="$BUILD" \
 --force-arch x86_64 --post-install jenkins/rpm/v2/post_install_script.sh \
 --post-uninstall jenkins/rpm/v2/post_uninstall_script.sh
 
 # Above --force-arch flag and below code is just for backward compatibility.
 # Can be removed once integration is done with RE.
-rpm_file=`ls -1 ${DIST_DIR}/dist/ | grep x86_64 | grep -v debug`
+rpm_file=`ls -1 ${BASE_DIR}/dist/ | grep x86_64 | grep -v debug`
 mkdir -p ${BASE_DIR}/dist/rpmbuild/RPMS/x86_64
-cp ${DIST_DIR}/dist/${rpm_file} ${BASE_DIR}/dist/rpmbuild/RPMS/x86_64/
+cp ${BASE_DIR}/dist/${rpm_file} ${BASE_DIR}/dist/rpmbuild/RPMS/x86_64/
+
+cp -rf ${BASE_DIR}/dist/temp/ha.conf ${BASE_DIR}/conf/etc/v2/ha.conf
 
 cd ${BASE_DIR}/dist
-rm -rf $DIST_DIR
 
 echo "********** RPM ****************"
 realpath $(find -name "*.rpm")
