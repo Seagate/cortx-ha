c6ce6cbc970444a7e794a5446ad56315f679c58b
Ajay Paratmandali <ajay.paratmandali@seagate.com>
2021-07-01 22:37:54 -0600

    EOS-20986: Update pre disruptive upgrade
    Signed-off-by: Ajay Paratmandali <ajay.paratmandali@seagate.com>

--- ha/setup/pre_disruptive_upgrade.py
+++ ha/setup/pre_disruptive_upgrade.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # Copyright (c) 2021 Seagate Technology LLC and/or its Affiliates
 #
 # This program is free software: you can redistribute it and/or modify it under the
@@ -15,24 +17,24 @@
 
 """Module to do pre-upgrade routines in scope of Disruptive Upgrade feature."""
 
-import argparse
 import os
+import sys
 import shlex
+import time
+import argparse
 import subprocess
+import traceback
 import tarfile
 from glob import glob
 from shutil import copytree, rmtree
 from time import gmtime, strftime
 from typing import Any
-from xml.etree import ElementTree
-from xml.etree.ElementTree import Element
 
 from cortx.utils.log import Log
-from ha.const import (BACKUP_DEST_DIR_CONF, BACKUP_DEST_DIR_CONSUL,
-                      CONFIG_DIR, RA_LOG_DIR)
+from ha.const import (BACKUP_DEST_DIR_CONF, BACKUP_DEST_DIR_CONSUL, CONFIG_DIR, PCS_RESOURCE_REFRESH, LIST_PCS_RESOURCES,
+                    PCS_CLUSTER_STANDBY, RA_LOG_DIR, PCS_CLEANUP, PCS_CLUSTER_STATUS, PCS_FAILCOUNT_STATUS, PCS_DELETE_RESOURCE)
 from ha.core.error import UpgradeError
 from ha.execute import SimpleCommand
-
 
 def backup_consul(filename: str = "consul-kv-dump.json", dst: str = BACKUP_DEST_DIR_CONSUL) -> None:
     """
@@ -68,7 +70,6 @@ def backup_consul(filename: str = "consul-kv-dump.json", dst: str = BACKUP_DEST_
     if cp.returncode:
         raise UpgradeError("Consul export failed with error {}".format(cp.stderr.decode()))
 
-
 def backup_configuration(src: str = CONFIG_DIR, dst: str = BACKUP_DEST_DIR_CONF) -> None:
     """
     Backup HA configuration.
@@ -89,8 +90,7 @@ def backup_configuration(src: str = CONFIG_DIR, dst: str = BACKUP_DEST_DIR_CONF)
             rmtree(dst)
         copytree(src, dst)
     except Exception as err:
-        raise UpgradeError("Failed to create backup of HA config") from err
-
+        raise UpgradeError("Failed to create backup of HA config")
 
 def cluster_standby_mode() -> None:
     """
@@ -102,18 +102,26 @@ def cluster_standby_mode() -> None:
         UpgradeError
     """
     Log.info("Set cluster to standby mode")
-    standby_cmd = "pcs node standby --all --wait=600"
+    Log.info("Please wait, standby can take max 20 to 30 min.")
+    standby_cmd = f"{PCS_CLUSTER_STANDBY} --wait=600"
     try:
         SimpleCommand().run_cmd(standby_cmd)
     except Exception as err:
-        raise UpgradeError("Cluster standby operation failed") from err
+        raise UpgradeError("Cluster standby operation failed")
 
-
-def _get_cib_xml() -> Element:
-    """Call `pcs cluster cib` and return XML object for further parsing."""
-    output, _, _ = SimpleCommand().run_cmd("pcs cluster cib")
-    return ElementTree.fromstring(output)
-
+def _get_resource_list() -> list:
+    """Get list of resource"""
+    resources: list = []
+    # clear history before getting list of resource
+    SimpleCommand().run_cmd(PCS_RESOURCE_REFRESH)
+    output, _, _ = SimpleCommand().run_cmd(LIST_PCS_RESOURCES, check_error=False)
+    if "NO resources" in output:
+        return resources
+    for resource in output.split("\n"):
+        res = resource.split(":")[0]
+        if res != "" and res not in resources:
+            resources.append(res)
+    return resources
 
 def delete_resources() -> None:
     """
@@ -123,17 +131,41 @@ def delete_resources() -> None:
         UpgradeError
     """
     try:
-        root = _get_cib_xml()
-        resources = [e.attrib["id"] for e in root.findall(".//lrm_resource")
-                     if "id" in e.attrib]
+        resources = _get_resource_list()
         Log.info(f"Going to delete following resources: {resources}")
-
         for r in resources:
-            Log.info(f"Deleting {r}")
-            SimpleCommand().run_cmd(f"pcs resource delete {r}")
+            Log.info(f"Deleting resource {r}")
+            SimpleCommand().run_cmd(PCS_DELETE_RESOURCE.replace("<resource>", r))
+        SimpleCommand().run_cmd(PCS_CLEANUP)
+        Log.info("Wait 2 min till all resource deleted.")
+        is_resource_deleted(120)
     except Exception as err:
-        raise UpgradeError("Resource deletion failed") from err
+        raise UpgradeError("Resource deletion failed")
 
+def check_cluster_health() -> None:
+    """ Check cluster status and make sure cluster is healthy """
+    # Check if cluster running
+    _, _, rc = SimpleCommand().run_cmd(PCS_CLUSTER_STATUS, check_error=False)
+    if rc != 0:
+        raise UpgradeError("Cluster is not running on current node")
+    output, _, _ = SimpleCommand().run_cmd(PCS_FAILCOUNT_STATUS)
+    if "INFINITY" in output:
+        raise UpgradeError(f"Cluster is not stable, some resource are not healthy. {output}")
+
+def is_resource_deleted(timeout) -> None:
+    """ Check if pre disruptive upgrade is successful """
+    base_wait = 5
+    while timeout > 0:
+        resources = _get_resource_list()
+        Log.info(f"Waiting for {str(timeout)} to delete resources {resources}.")
+        if len(resources) == 0:
+            Log.info("All resource deleted successfully.")
+            break
+        time.sleep(base_wait)
+        timeout -= base_wait
+    resources = _get_resource_list()
+    if len(resources) != 0:
+        raise UpgradeError(f"Failed to delete resource. Remaining resources {resources} ...")
 
 def _parse_arguments() -> Any:
     """
@@ -146,23 +178,23 @@ def _parse_arguments() -> Any:
     parser.add_argument("--backup-consul", action="store_true", help="Do consul backup")
     return parser.parse_args()
 
-
 def _main() -> None:
     args = _parse_arguments()
-
     Log.init(service_name="pre_disruptive_upgrade", log_path=RA_LOG_DIR, level="INFO")
-
     Log.info("Script invoked as executable with params: {}".format(vars(args)))
-
+    check_cluster_health()
     if args.backup_consul:
         backup_consul()
-
     backup_configuration()
-
     cluster_standby_mode()
-
     delete_resources()
 
-
 if __name__ == "__main__":
-    _main()
+    try:
+        _main()
+        sys.stdout.write("Successfully completed pre disruptive upgrade")
+        sys.exit(0)
+    except Exception as e:
+        Log.error(f"Failed pre disruptive upgrade. Error: {e}. Traceback:\n {traceback.format_exc()}")
+        sys.stderr.write("Failed to perform pre disruptive upgrade. Please check log for more detail.")
+        sys.exit(1)
