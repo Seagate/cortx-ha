6d59f497160ccae2fb96a6d02bc31c1acc821ce4
Ajay Paratmandali <66459479+ajay-paratmandali@users.noreply.github.com>
2021-07-14 14:19:57 +0530

    Fix dev env (#475)
    * Fix dev env
    
    Signed-off-by: Ajay Paratmandali <ajay.paratmandali@seagate.com>
    
    * Add licence to dev env
    
    Signed-off-by: Ajay Paratmandali <ajay.paratmandali@seagate.com>

--- jenkins/dev_env/build_rpm.sh
+++ jenkins/dev_env/build_rpm.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+# Copyright (c) 2021 Seagate Technology LLC and/or its Affiliates
+#
+# This program is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Affero General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <https://www.gnu.org/licenses/>. For any questions
+# about this software or licensing, please email opensource@seagate.com or
+# cortx-questions@seagate.com.
+
 BASE_DIR=$(realpath "$(dirname $0)")
 
 source ${BASE_DIR}/conf/read_conf.sh $1
@@ -19,21 +34,18 @@ USER=${config[USER]}
 
 echo "PATH: ${REPO_PATH}, USER: ${USER}"
 
-[ $REPO_PATH == "--clean" ] && {
-    echo "Cleaning Yum"
-    rm -rf /etc/yum.repos.d/cortx-*
+mkdir -p /var/lib/ha_env
+
+ls /var/lib/ha_env/ | grep yum_init || {
+    # Configure cortx-py-utils
+    yum-config-manager --add-repo ${THIRD_PARTY}
+    yum-config-manager --add-repo ${CORTX_ISO}
     yum clean all
-    exit 0
+    rpm --import ${GPG_CHECK}
 }
 
-# Configure cortx-py-utils
-yum-config-manager --add-repo ${THIRD_PARTY}
-yum-config-manager --add-repo ${CORTX_ISO}
-yum clean all
-rpm --import ${GPG_CHECK}
-
 yum install -y gcc rpm-build python36 python36-pip python36-devel python36-setuptools openssl-devel libffi-devel  --nogpgcheck
-yum group install "Development Tools"
+yum group -y install "Development Tools" --nogpgcheck
 
 python3 -m pip install -r https://raw.githubusercontent.com/Seagate/cortx-utils/main/py-utils/python_requirements.txt
 python3 -m pip install -r https://raw.githubusercontent.com/Seagate/cortx-utils/main/py-utils/python_requirements.ext.txt
@@ -52,3 +64,5 @@ echo "${REPO_PATH}/dist/rpmbuild/RPMS/x86_64/cortx-ha-2.0.0-*.x86_64.rpm"
 
 yum remove -y cortx-ha
 yum install -y ${REPO_PATH}/dist/rpmbuild/RPMS/x86_64/cortx-ha-2.0.0-*.x86_64.rpm
+
+touch /var/lib/ha_env/yum_init
