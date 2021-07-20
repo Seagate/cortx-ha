5e843907824e728352fbfe8f6f7fe587d2453608
Ajay Paratmandali <ajay.paratmandali@seagate.com>
2021-07-20 01:57:15 -0600

    Update system health
    Signed-off-by: Ajay Paratmandali <ajay.paratmandali@seagate.com>

---
+++ ha/util/message_bus.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2021 Seagate Technology LLC and/or its Affiliates
+#
+# This program is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Affero General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <https://www.gnu.org/licenses/>. For any questions
+# about this software or licensing, please email opensource@seagate.com or
+# cortx-questions@seagate.com.
+
+import json
+
+from cortx.utils.message_bus import MessageBusAdmin
+from cortx.utils.message_bus.error import MessageBusError
+from cortx.utils.message_bus import MessageProducer
+
+class MessageBusProducer:
+    ADMIN_ID = "admin"
+    PRODUCER_METHOD = "sync"
+
+    def __init__(self, producer_id: str, message_type: str, partitions: int = 1):
+        """
+        Register message types with message bus.
+
+        Args:
+            producer_id (str): producer id.
+            message_types (str): Message type.
+            partitions (int, optional): No. of partitions. Defaults to 1.
+
+        Raises:
+            MessageBusError: Message bus error.
+        """
+        try:
+            admin = MessageBusAdmin(admin_id=MessageBusProducer.ADMIN_ID)
+            admin.register_message_type(message_types=[message_type], partitions=partitions)
+        except MessageBusError as e:
+            if "ALREADY_EXISTS" not in e.desc:
+                raise Exception(f"Failed to register {message_type} with message bus. Error: {e.desc}")
+
+        self.producer = MessageProducer(producer_id=producer_id, message_type=message_type, method=MessageBusProducer.PRODUCER_METHOD)
+
+    def produce(self, message: any):
+        """
+        Produce message to message bus.
+
+        Args:
+            message (dict): Message.
+        """
+        if isinstance(message, dict):
+            self.producer.send([json.dumps(message)])
+        elif isinstance(message, str):
+            self.producer.send([message])
+        else:
+            raise Exception(f"Invalid type of message {message}")
+
+class MessageBusConsumer:
+    pass
