f5fe4ae01c049339f766a9786c407b1b6a9851e1
Akash Dudhane <akash.a.dudhane@seagate.com>
2021-10-18 10:46:41 -0600

    Added K8SFilter class
    Signed-off-by: Akash Dudhane <akash.a.dudhane@seagate.com>

--- ha/test/integration/health_monitor/test_k8s_publisher.py
+++ ha/test/integration/health_monitor/test_k8s_publisher.py
@@ -14,18 +14,20 @@
 # with this program. If not, see <https://www.gnu.org/licenses/>. For any questions
 # about this software or licensing, please email opensource@seagate.com or
 # cortx-questions@seagate.com.
+import json
 import os
 import sys
 import pathlib
 import time
 
-from ha.core.event_analyzer.filter_event import FiletrEvent
 from ha.alert.K8s_alert import K8SAlert
+from ha.core.event_manager.model.action_event import RecoveryActionEvent
 from ha.core.system_health.model.health_event import HealthEvent
 from ha.core.event_manager.event_manager import EventManager
 from ha.core.event_manager.subscribe_event import SubscribeEvent
 from ha.util.message_bus import MessageBus, CONSUMER_STATUS
 from ha.const import K8S_ALERT_RESOURCE_TYPE, K8S_ALERT_STATUS
+from ha.core.event_analyzer.filter.filter import K8SFilter
 
 sys.path.append(os.path.join(os.path.dirname(pathlib.Path(__file__)), '..', '..', '..'))
 
@@ -41,18 +43,19 @@ if __name__ == '__main__':
     try:
         print("********Event Publisher********")
         event_manager = EventManager.get_instance()
-        component = "csm"
+        k8s_filter = K8SFilter()
+        component = "hare"
         resource_type = K8S_ALERT_RESOURCE_TYPE.RESOURCE_TYPE_POD.value
         state = K8S_ALERT_STATUS.STATUS_FAILED.value
-        message_type = event_manager.subscribe('csm', [SubscribeEvent(resource_type, [state])])
+        message_type = event_manager.subscribe('hare', [SubscribeEvent(resource_type, [state])])
         print(f"Subscribed {component}, message type is {message_type}")
         k8s_event = K8SAlert("cortx", "node2", "cortx-data123", K8S_ALERT_STATUS.STATUS_FAILED.value, K8S_ALERT_RESOURCE_TYPE.RESOURCE_TYPE_POD.value, "16215909572")
-        health_event = HealthEvent(k8s_event)
-        filetr_event = FiletrEvent()
-        recovery_action_event = filetr_event.process_event(health_event)
-        if recovery_action_event is not None:
+        if k8s_filter.filter_event(json.dumps(k8s_event.__dict__)):
+            health_event = HealthEvent(k8s_event)
+            recovery_action_event = RecoveryActionEvent(health_event)
             event_manager.publish(recovery_action_event)
         else:
+            print("Event is dropped as it doesn't meet criteria")
             sys.exit(0)
         print("Consuming the action event")
         message_consumer = MessageBus.get_consumer(consumer_id="1",
