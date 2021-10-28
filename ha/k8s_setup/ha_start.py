358b92a13e8b66266b9eeb9821ef45ca01ad1aaa
Akash Dudhane <akash.a.dudhane@seagate.com>
2021-10-28 06:06:03 -0600

    Added exception handling for invalid service
    Signed-off-by: Akash Dudhane <akash.a.dudhane@seagate.com>

--- ha/k8s_setup/ha_start.py
+++ ha/k8s_setup/ha_start.py
@@ -10,15 +10,31 @@ my_parser = argparse.ArgumentParser(prog="ha_start",
                                     epilog='Fault tolerance is containerized! :)'
                                     )
 
-my_parser.add_argument('-s', help='service_name', required=True, action='store')
-my_parser.add_argument('-c', help='config file', required=False, action='store')
+my_parser.add_argument('--services', '-s', help='service_name', required=True, action='store')
+my_parser.add_argument('--config', '-c', help='config file', required=False, action='store')
 args = my_parser.parse_args()
 
+service_entry_mapping = {
+                        'health_monitor': '/usr/lib/python3.6/site-packages/ha/core/health_monitor/health_monitord.py',
+                        'fault_tolerance': '/usr/lib/python3.6/site-packages/ha/fault_tolerance/fault_tolerance_driver.py'
+                        }
+
+def usage(prog: str):
+        """
+        Print usage instructions
+        """
+        sys.stderr.write(
+            f"usage: {prog} [-h] <--config url> <--services service to start>...\n"
+            f"where:\n"
+            f"--services fault_tolerance, k8s_monitor\n"
+            f"--config   Config URL\n")
+
 try:
-    if args.s == 'fault_tolerance':
-        driver_process= Popen(['/usr/bin/python3', '/usr/lib/python3.6/site-packages/ha/fault_tolerance/fault_tolerance_driver.py'], shell=False, stdout=PIPE, stderr=PIPE) # nosec
-    elif args.s == 'health_monitor':
-        driver_process= Popen(['/usr/bin/python3', '/usr/lib/python3.6/site-packages/ha/core/health_monitor/health_monitord.py'], shell=False, stdout=PIPE, stderr=PIPE) # nosec
+    if args.services not in service_entry_mapping:
+        print('Please provide a valid service name')
+        usage('ha_start')
+        sys.exit(22)
+    driver_process = Popen(['/usr/bin/python3', service_entry_mapping[args.services]], shell=False, stdout=PIPE, stderr=PIPE) # nosec
+
 except Exception as proc_err:
     sys.stderr.write(f'Driver execution stopped because of some reason: {proc_err}')
-
