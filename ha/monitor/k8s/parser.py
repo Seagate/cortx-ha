d70f5062d914fa1cf83f9c3dd9766a6bf9f1e6d3
root <root@ssc-vm-g3-rhev4-2370.colo.seagate.com>
2021-11-29 06:33:00 -0700

    EOS-26559 [K8S monitor] resolved comments
    Signed-off-by: root <root@ssc-vm-g3-rhev4-2370.colo.seagate.com>

--- ha/monitor/k8s/parser.py
+++ ha/monitor/k8s/parser.py
@@ -47,23 +47,10 @@ class NodeEventParser(ObjectParser):
         alert.resource_type = self._type
         alert.timestamp = str(int(time.time()))
 
-        # Get value of machine id key (path of the machine id in k8s event)
-        machine_id_key = Conf.get(const.HA_GLOBAL_INDEX, f"MONITOR{_DELIM}machine_id_key")
-
-        # loop over keys and check if exist then get the value.
-        # this code is flexible can be used for any key in event for example
-        # if value at the place event[raw_object][metadat][name] then input key will be 'metadata/name'
-        # note if the key is fixed cannot chnage then can use constant here also insted of parsing
-        machine_id_keys = machine_id_key.split('/')
-        machine_id = an_event[K8SEventsConst.RAW_OBJECT]
-        for key in machine_id_keys:
-            if key != None and isinstance(machine_id, dict) and key in machine_id:
-                machine_id = machine_id[key]
-        if  machine_id is not None and not isinstance(machine_id, dict):
-            alert.resource_name = machine_id
-
         if K8SEventsConst.TYPE in an_event:
             alert.event_type = an_event[K8SEventsConst.TYPE]
+        if K8SEventsConst.NAME in an_event[K8SEventsConst.RAW_OBJECT][K8SEventsConst.METADATA]:
+            alert.resource_name = an_event[K8SEventsConst.RAW_OBJECT][K8SEventsConst.METADATA][K8SEventsConst.NAME]
 
         ready_status = None
         try:
@@ -119,9 +106,21 @@ class PodEventParser(ObjectParser):
         alert.resource_type = self._type
         alert.timestamp = str(int(time.time()))
 
-        labels = an_event[K8SEventsConst.RAW_OBJECT][K8SEventsConst.METADATA][K8SEventsConst.LABELS]
-        if K8SEventsConst.MACHINEID in labels:
-            alert.resource_name = labels[K8SEventsConst.MACHINEID]
+        # Get value of machine id key (path of the machine id in k8s event)
+        machine_id_key = Conf.get(const.HA_GLOBAL_INDEX, f"MONITOR{_DELIM}machine_id_key")
+
+        # loop over keys and check if exist then get the value.
+        # this code is flexible can be used for any key in event for example
+        # if value at the place event[raw_object][metadat][name] then input key will be 'metadata/name'
+        # note if the key is fixed cannot chnage then can use constant here also insted of parsing
+        machine_id_keys = machine_id_key.split('/')
+        machine_id = an_event[K8SEventsConst.RAW_OBJECT]
+        for key in machine_id_keys:
+            if key != None and isinstance(machine_id, dict) and key in machine_id:
+                machine_id = machine_id[key]
+        if  machine_id is not None and not isinstance(machine_id, dict):
+            alert.resource_name = machine_id
+
         if K8SEventsConst.TYPE in an_event:
             alert.event_type = an_event[K8SEventsConst.TYPE]
         if K8SEventsConst.NODE_NAME in an_event[K8SEventsConst.RAW_OBJECT][K8SEventsConst.SPEC]:
