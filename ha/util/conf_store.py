c93c27468c5f9cccc517e1075ca7a6e1ab084a69
shaileshsvc <92013295+shaileshsvc@users.noreply.github.com>
2022-01-13 11:32:57 +0530

    Fault k8s (#627)
    * EOS-26969 Implement logic to get data and storage pod information from ConfStore (#625)
    
    * Changes for EOS-26969 Implement logic to get data and storage pod information from ConfStore
    
    cluster cardinality created using the confstore APIs
    
    Signed-off-by: ArchanaLimaye <archana.limaye@seagate.com>
    
    * Changes for EOS-26969 Implement logic to get data and storage pod information from ConfStore
    
    Test file added
    
    Signed-off-by: ArchanaLimaye <archana.limaye@seagate.com>
    
    * Changes for EOS-26969 Implement logic to get data and storage pod information from ConfStore
    
    codacy fixes
    
    Signed-off-by: ArchanaLimaye <archana.limaye@seagate.com>
    
    * Changes for EOS-26969 Implement logic to get data and storage pod information from ConfStore
    
    REview comments handled
    
    Signed-off-by: ArchanaLimaye <archana.limaye@seagate.com>
    
    * EOS-26971: Modify monitor service to use machine ids fetched from confstore to filter the API server alert (#626)
    
    * EOS-26971: Modify monitor service to use machine ids fetched from confstore to filter the API server alert
    
    - Use conf store search API HA wrapper class to get the cluster
      cardinality(list of machind_ids).
    - Use cortx.io/machine-id label of a Node to match its value with machine_id
      list fetched from above API to filter the alert.
    - Remove hardcoded label from ha_setup
    
    Signed-off-by: Madhura Mande <madhura.mande@seagate.com>
    
    * EOS-26971: Modify monitor service to use machine ids fetched from confstore to filter the API server alert
    
    - Modification after rebase.
    - Remove testing code
    - Fix review comments
    
    Signed-off-by: Madhura Mande <madhura.mande@seagate.com>
    
    Co-authored-by: ArchanaLimaye <62940238+ArchanaLimaye@users.noreply.github.com>
    Co-authored-by: Madhura Mande <madhura.mande@seagate.com>
    Signed-off-by: ArchanaLimaye <archana.limaye@seagate.com>

--- ha/util/conf_store.py
+++ ha/util/conf_store.py
@@ -21,8 +21,11 @@ Wrappeprs for using the search APIs from conf store
 
 import json
 
+from cortx.utils.conf_store import Conf
+from cortx.utils.cortx.const import Const
 from ha.core.config.config_manager import ConfigManager
 from ha.k8s_setup.const import CLUSTER_CARDINALITY_KEY, CLUSTER_CARDINALITY_NUM_NODES, CLUSTER_CARDINALITY_LIST_NODES
+from ha.k8s_setup.const import NODE_CONST, SERVICE_CONST
 from cortx.utils.log import Log
 from ha.core.error import ClusterCardinalityError
 
@@ -41,6 +44,16 @@ class ConftStoreSearch:
         Get machine ids for data pods: returns list of machine ids.
         """
         machine_ids = []
+        # Sample output of the serach API
+        # ex: Conf.search("cortx", "node", "services", Const.SERVICE_MOTR_IO.value)
+        # ['node>5f3dc3a153454a918277ee4a2c57f36b>components[1]>services[0]',
+        # 'node>6203a14bde204e8ea798ad9d42583fb5>components[1]>services[0]', 'node>8cc8b13101e34b3ca1e51ed6e3228d5b>components[1]>services[0]']
+
+        data_pod_keys = Conf.search(index, NODE_CONST, SERVICE_CONST, Const.SERVICE_MOTR_IO.value)
+        for key in data_pod_keys:
+            machine_id = key.split('>')[1]
+            # Add machine id to list
+            machine_ids.append(machine_id)
         return machine_ids
 
     def _get_server_pods(self, index):
@@ -48,6 +61,11 @@ class ConftStoreSearch:
         Get machine ids for server pods: returns list of machine ids.
         """
         machine_ids = []
+        server_pod_keys = Conf.search(index, NODE_CONST, SERVICE_CONST, Const.SERVICE_S3_HAPROXY.value)
+        for key in server_pod_keys:
+            machine_id = key.split('>')[1]
+            # Add machine id to list
+            machine_ids.append(machine_id)
         return machine_ids
 
     def get_cluster_cardinality(self):
@@ -85,8 +103,10 @@ class ConftStoreSearch:
 
         Log.info(f"Cluster cardinality: number of nodes {num_pods}, machine ids for nodes {watch_pods} ")
 
-        # Update the same to consul
-        # if KV already present, it will be modified.
+        if num_pods == 0:
+            Log.warn(f"Possible cluster cardinality issue; number of pods to be watched {num_pods}")
+
+        # Update the same to consul; if KV already present, it will be modified.
         cluster_cardinality_key = CLUSTER_CARDINALITY_KEY
         cluster_cardinality_value = {CLUSTER_CARDINALITY_NUM_NODES: num_pods, CLUSTER_CARDINALITY_LIST_NODES : watch_pods }
         self._confstore.update(cluster_cardinality_key, json.dumps(cluster_cardinality_value))
