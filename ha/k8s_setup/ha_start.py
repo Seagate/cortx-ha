91b4d754e0b223a25bd311b50686d08c3567213e
Ajay Paratmandali <ajay.paratmandali@seagate.com>
2022-02-03 20:54:02 -0700

    Provide service restrat for container
    Signed-off-by: Ajay Paratmandali <ajay.paratmandali@seagate.com>

--- ha/k8s_setup/ha_start.py
+++ ha/k8s_setup/ha_start.py
@@ -37,24 +37,50 @@ def handle_signal(signum, frame):
     Signal handler call beck function, to forward the received signal to driver process
     """
     sys.stdout.write(f"Signal {signum} at frame {frame} received.")
-    sys.stdout.write(f"Sending {signum} to driver process pid: {driver_process.pid}.")
-    driver_process.send_signal(signum)
+    # If received SIGUSR1 then restart service
+    if signal.SIGUSR1.value == signum:
+        print("\nCaught signal.SIGUSR1, Restarting process")
+        driver_process.kill()
+        set_signals()
+        start_process()
+        driver_process.wait()
+    else:
+        sys.stdout.write(f"Sending {signum} to driver process pid: {driver_process.pid}.")
+        driver_process.kill()
+        raise Exception(f"Received {signum} signals, terminating process.\n")
 
-try:
-    if args.services not in service_entry_mapping:
-        sys.stdout.write('Please provide a valid service name')
-        usage('ha_start')
-        sys.exit(22)
-    # setting Signal handlers for all standered signals
+def start_process():
+    """
+    Start Process
+    """
+    global driver_process
+    driver_process = Popen(
+        ['/usr/bin/python3', service_entry_mapping[args.services]],
+        shell=False, stdout=PIPE, stderr=PIPE) # nosec
+    sys.stdout.write(f"The driver process with pid {driver_process.pid}, and args {driver_process.args} started successfully.\n")
+    driver_process.wait()
+
+def set_signals():
+    # setting Signal handlers for all standard signals
     signal.signal(signal.SIGABRT, handle_signal)
     signal.signal(signal.SIGFPE, handle_signal)
     signal.signal(signal.SIGILL, handle_signal)
     signal.signal(signal.SIGINT, handle_signal)
     signal.signal(signal.SIGSEGV, handle_signal)
     signal.signal(signal.SIGTERM, handle_signal)
+    signal.signal(signal.SIGUSR1, handle_signal)
 
-    driver_process = Popen(['/usr/bin/python3', service_entry_mapping[args.services]], shell=False, stdout=PIPE, stderr=PIPE) # nosec
-    sys.stdout.write(f"The driver process with pid {driver_process.pid}, and args {driver_process.args} started successfully.")
-    exit(driver_process.wait())
-except Exception as proc_err:
-    sys.stderr.write(f'Driver execution stopped because of some reason: {proc_err}')
+if __name__ == "__main__":
+    try:
+        if args.services not in service_entry_mapping:
+            sys.stdout.write('Please provide a valid service name')
+            usage('ha_start')
+            sys.exit(22)
+
+        set_signals()
+        start_process()
+        import time
+        while True:
+            time.sleep(1000)
+    except Exception as proc_err:
+        sys.stderr.write(f'Driver execution stopped because of some reason: {proc_err}\n')
