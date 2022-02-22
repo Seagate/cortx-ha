dc45e3eccc09b0810ad298b824aba3e0a4346533
MARIYAPPAN PONNUSAMY <mariyappan.ponnusamy@seagate.com>
2022-02-22 08:22:30 -0700

    Handle Init Pod and Termination Pod During Pod Deletion
    Signed-off-by: MARIYAPPAN PONNUSAMY <mariyappan.ponnusamy@seagate.com>

--- ha/monitor/k8s/object_monitor.py
+++ ha/monitor/k8s/object_monitor.py
@@ -181,41 +181,47 @@ class ObjectMonitor(threading.Thread):
     def _is_published_alert(self, alert) -> bool:
         """
         Check incoming alert is already published or not.
-        If incoming alert is not found in published alerts then,
-        it is a new alert to publish.
-        self._published_alerts = { resource_name: [alert1, alert2] }
+        If incoming alert is not found in published alerts, then
+        it is a new alert to publish. Alert will be stored like,
+        self._published_alerts = {
+            resource_name: { generation_id : alert }
+            }
 
         Returns:
             True if it is published already
             False if it is a new alert
         """
         incoming_alert = alert.to_dict().copy()
+        incoming_resource_name = incoming_alert.get('_resource_name')
+        incoming_generation_id = incoming_alert.get('_generation_id')
+
+        if incoming_alert.get('_timestamp') is None or incoming_resource_name is None:
+            # Consider any other alerts to be published
+            return False
 
         # Alert with no status change also has new timestamp.
         # So timestamp field should be ignored for validation.
-        if not incoming_alert.get('_timestamp') is not None:
-            return False
         del incoming_alert['_timestamp']
 
         # Remove user added fields those aren't exist in raw event
         if incoming_alert.get('_is_status') is not None:
             del incoming_alert['_is_status']
 
-        resource_name = incoming_alert.get('_resource_name')
+        incoming_alert_msg = json.dumps(incoming_alert, sort_keys=True)
+        published_alerts = self._published_alerts.get(incoming_resource_name)
 
-        if resource_name:
-            incoming_alert_msg = json.dumps(incoming_alert, sort_keys=True)
-            published_alerts = self._published_alerts.get(resource_name)
-            if published_alerts and isinstance(published_alerts, list):
-                if incoming_alert_msg in published_alerts:
-                    return True
-                else:
-                    published_alerts.append(incoming_alert_msg)
+        if published_alerts:
+            incoming_generation_id = incoming_alert.get('_generation_id')
+            published_alert = published_alerts.get(incoming_generation_id)
+            if incoming_alert_msg == published_alert:
+                # Published already
+                return True
             else:
-                self._published_alerts[resource_name] = []
-                self._published_alerts[resource_name].append(incoming_alert_msg)
+                # New alert
+                self._published_alerts[incoming_resource_name][incoming_generation_id] = incoming_alert_msg
         else:
-            # Consider any other alerts to be published
-            pass
+            self._published_alerts[incoming_resource_name] = {
+                incoming_generation_id: incoming_alert_msg
+                }
 
         return False
