697acc424baedb62998cd3aedbcd190dfa4c7be3
Mukhtar Inamdar <mukhtar.p.inamdar@seagate.com>
2022-03-15 04:47:15 -0600

    CORTX-29176 removing changes that are not required in this PR
    Signed-off-by: Mukhtar Inamdar <mukhtar.p.inamdar@seagate.com>

--- ha/k8s_setup/ha_start.py
+++ ha/k8s_setup/ha_start.py
@@ -5,8 +5,6 @@ import signal
 from subprocess import Popen, PIPE, STDOUT # nosec
 import sys
 
-AUTO_RESTART = 1
-
 driver_process = None
 
 my_parser = argparse.ArgumentParser(prog="ha_start",
@@ -16,7 +14,6 @@ my_parser = argparse.ArgumentParser(prog="ha_start",
 
 my_parser.add_argument('--services', '-s', help='service_name', required=True, action='store')
 my_parser.add_argument('--config', '-c', help='config file', required=False, action='store')
-my_parser.add_argument('--debug', '-d', help='debug mode {0|1}', type=int, nargs='?', const=1, default=0, required=False, action='store')
 args = my_parser.parse_args()
 
 service_entry_mapping = {
@@ -33,8 +30,7 @@ def usage(prog: str):
             f"usage: {prog} [-h] <--config url> <--services service to start>...\n"
             f"where:\n"
             f"--services health_monitor, fault_tolerance, k8s_monitor\n"
-            f"--config   Config URL\n"
-            f"--debug debug levels\n\t0 : no debug\n\t1 : auto restart child service")
+            f"--config   Config URL\n")
 
 def handle_signal(signum, frame):
     """
@@ -44,17 +40,7 @@ def handle_signal(signum, frame):
     sys.stdout.write(f"Sending {signum} to driver process pid: {driver_process.pid}.")
     driver_process.send_signal(signum)
 
-def start_driver_process():
-    global driver_process
-    driver_process = Popen(['/usr/bin/python3', service_entry_mapping[args.services]], shell=False, stdout=PIPE, stderr=STDOUT) # nosec
-    sys.stdout.write(f"The driver process with pid {driver_process.pid}, and args {driver_process.args} started successfully.")
-
 try:
-    sys.stdout.write("Starting driver process with arguments: "+str(args))
-    if args.debug not in [0,1]:
-        sys.stderr.write('Please provide a valid debug value.')
-        usage('ha_start')
-        sys.exit(22)
     if args.services not in service_entry_mapping:
         sys.stdout.write('Please provide a valid service name')
         usage('ha_start')
@@ -67,19 +53,15 @@ try:
     signal.signal(signal.SIGSEGV, handle_signal)
     signal.signal(signal.SIGTERM, handle_signal)
 
-    start_driver_process()
+    driver_process = Popen(['/usr/bin/python3', service_entry_mapping[args.services]], shell=False, stdout=PIPE, stderr=STDOUT) # nosec
+
+    sys.stdout.write(f"The driver process with pid {driver_process.pid}, and args {driver_process.args} started successfully.")
 
     # Poll process.stdout to show stdout live
     while True:
         output = driver_process.stdout.readline()
         if driver_process.poll() is not None:
-            if((args.debug&AUTO_RESTART)!=0):
-                if output:
-                    print(output.strip().decode("utf-8"))
-                sys.stdout.write(f'Warning: Driver \'{args.services}\' exited with return code: {driver_process.poll()}, restarting again.')
-                start_driver_process()
-            else:
-                break
+            break
         if output:
             print(output.strip().decode("utf-8"))
     exit(driver_process.poll())
