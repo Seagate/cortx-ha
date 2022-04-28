46d5591e0d7c2c91c6da03759fd990d3389099fd
Madhura Mande <madhura.mande@seagate.com>
2022-04-28 13:28:56 +0530

    Cortx-29991: Event Subscription: API enhancements (#695)
    * CORTX-29991: Event Subscription: API enhancements
    
    - unsubscribe API enhancements
    - Enhancements in the APIs which are required to delete consul keys
      in case of unsubscription
    
    Signed-off-by: Madhura Mande <madhura.mande@seagate.com>
    
    * CORTX-29991: Event Subscription: API enhancements
    
    - Remove logger error statement which is not required
    
    Signed-off-by: Madhura Mande <madhura.mande@seagate.com>
    
    * CORTX-29991: Event Subscription: API enhancements
    
    - Remove one logger error statement which is not required
    
    Signed-off-by: Madhura Mande <madhura.mande@seagate.com>
    
    * Cortx-29991: Event Subscription: API enhancements
    
    - Add Integration test for Unsubscribe API
    
    Signed-off-by: Madhura Mande <madhura.mande@seagate.com>
    
    * Cortx-29991: Event Subscription: API enhancements
    
    - Fix codacy issue
    
    Signed-off-by: Madhura Mande <madhura.mande@seagate.com>

--- ha/test/integration/health_monitor/test_event_subscriber.py
+++ ha/test/integration/health_monitor/test_event_subscriber.py
@@ -53,26 +53,21 @@ class TestEventManager(unittest.TestCase):
     def test_subscriber_with_functional_type(self):
         # pod event with functional type
         self.event_manager.subscribe(self.component, [self.pod_event_with_func_type])
-        # TODO: CORTX-29991 will validate unsubscribe
-        #self.event_manager.unsubscribe(self.component, [self.pod_event_with_func_type])
+        self.event_manager.unsubscribe(self.component, [self.pod_event_with_func_type])
 
     def test_subscriber_without_functional_type(self):
         # pod event without functional type
         self.event_manager.subscribe(self.component, [self.pod_event_without_func_type])
-        # TODO: CORTX-29991 will validate unsubscribe
-        #self.event_manager.unsubscribe(self.component, [self.pod_event_without_func_type])
+        self.event_manager.unsubscribe(self.component, [self.pod_event_without_func_type])
 
         # disk event without functional type
         self.event_manager.subscribe(self.component, [self.disk_event_without_func_type])
-        # TODO: CORTX-29991 will validate unsubscribe
-        #self.event_manager.unsubscribe(self.component, [self.disk_event_without_func_type])
+        self.event_manager.unsubscribe(self.component, [self.disk_event_without_func_type])
 
     def test_subscriber_with_functional_type_all(self):
         # pod event without functional type
         self.event_manager.subscribe(self.component, [self.pod_event_with_func_type_all])
-        # TODO: CORTX-29991 will validate unsubscribe
-        #self.event_manager.unsubscribe(self.component, [self.pod_event_with_func_type_all])
-
+        self.event_manager.unsubscribe(self.component, [self.pod_event_with_func_type_all])
 
 if __name__ == "__main__":
     unittest.main()
